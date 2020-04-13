b4d038556cde0231d4506d3455d45792a2bfa740
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-14 00:48:20 +0300

    3.11 phonebook full stack
    Add production build folder, just to show the progress since the changes are also deployed to heroku

---
+++ part3/Phonebook-backend/build/precache-manifest.a0748a33151b545b6a694007a35d50fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f8aee5f02f0b0b985111e5bcd4f7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "488da70a26cc109cc445",
+    "url": "/static/css/main.1d1f4f3f.chunk.css"
+  },
+  {
+    "revision": "67364ffd982831035b91",
+    "url": "/static/js/2.d464d379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d464d379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488da70a26cc109cc445",
+    "url": "/static/js/main.380ab0d2.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
