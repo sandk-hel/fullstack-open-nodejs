cedb8d03fec40db8c0cdf7bfaafb6350ff06a641
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-15 02:37:20 +0300

    Remove local fixture json

--- part3/Phonebook-backend/index.js
+++ part3/Phonebook-backend/index.js
@@ -18,29 +18,6 @@ app.use(morgan('tiny',
 app.use(morgan(':method :url :status :res[content-length]  - :response-time ms :bodyToken', 
             { skip:  (req, res) => !hasJSONBody(req, res) }))
 
-const persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/api/persons', (request, response) => {
     Person.find()
           .then(persons => persons.map(p => p.toJSON()))
