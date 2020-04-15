f500b5b0bb9d1e7fee5148ddef0b198f5bb840d5
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-15 09:12:46 +0300

    Deploying the database backend to production Add build folders

---
+++ part3/Phonebook-backend/build/precache-manifest.c8b95011f75ae5047696ba30525e97f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637aa2f915b75cf6ff3d7109316ceae4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23845a917d2bb9f1caf",
+    "url": "/static/css/main.1d1f4f3f.chunk.css"
+  },
+  {
+    "revision": "67364ffd982831035b91",
+    "url": "/static/js/2.d464d379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d464d379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23845a917d2bb9f1caf",
+    "url": "/static/js/main.238f23f2.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
