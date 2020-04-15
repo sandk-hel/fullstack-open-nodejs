154cf17211d45418b1373f14e54596816246ab3a
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-15 20:55:20 +0300

    4.4: helper functions and unit tests, step2
    Implement totalLikes methods and add test for it

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length == 0
+   ? 0
+   : blogs.reduce((sum, val) => sum + val.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
