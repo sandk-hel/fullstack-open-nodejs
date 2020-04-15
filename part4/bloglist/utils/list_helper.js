c77687330adcd56a9fa6e37b3ca2fd263800b1f6
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-16 00:22:02 +0300

    4.5  helper functions and unit tests, step3
    favoriteBlog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,20 @@ const totalLikes = (blogs) => {
    : blogs.reduce((sum, val) => sum + val.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const favBlog = blogs.sort((a, b) => a.likes < b.likes ? 1 : -1)[0]
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes
+  }  
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
