b1815204b561e7f80eb87a8d89b9a9b271d8b7ce
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-04 17:17:54 +0300

    8.4: Books of an author Modify the allBooks query so, that a user can give an optional parameter author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => { 
+      if (!args.author) {
+        return books 
+      }
+
+      return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => {
       return authors.map(author => {
         const authorBooks = books.filter(b => b.author === author.name)
