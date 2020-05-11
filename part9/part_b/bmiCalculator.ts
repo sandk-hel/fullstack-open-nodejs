ee196e590d4f2198f7ff12aa034523debacd3801
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-12 01:09:09 +0300

    9.1 Body mass index
    - bmiCalculator file with static method to calculate bmi

---
+++ part9/part_b/bmiCalculator.ts
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @param weight weight in kg
+ * @param height height in cm
+ */
+const calculateBmi = (weight: number, height: number): String => {
+  return 'Normal (healthy weight)'
+}
+
+console.log(calculateBmi(180, 74))
