f1947e0ec6d0bb5c847662f8453300248148b4e6
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-12 15:23:48 +0300

    9.5 WebBMI
    - Use bmiCalculator module and send bmi detail response

--- part9/part_b/index.ts
+++ part9/part_b/index.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import { calculateBmi, parseBmiQueries } from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => { 
+  try {
+    const { height, weight } = parseBmiQueries(req.query)
+    const bmi = calculateBmi(height, weight)
+    res.json({ height, weight, bmi })
+  } catch (exception) {
+    res.status(400).json({ error: exception.message })
+  }
 })
 
 const PORT = 3000
