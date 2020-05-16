6cb2fb52c9e196f669ba63c0f6ca81dc6a3a33c7
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-16 13:57:45 +0300

    9.19: patientor, step4 - OccupationalHealthCareEntry and HospitalEntry so that those conform with the example data

--- part9/Patientor/src/types.ts
+++ part9/Patientor/src/types.ts
@@ -1,13 +1,43 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
 
-}
-
-export interface Diagnose {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
+
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  specialist: string;
+  date: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string }
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthCareEntry
 
 export enum Gender {
   Male = "male",
